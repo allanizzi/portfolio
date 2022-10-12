a2a7f39346e4a24daac2f34c2989acd6689a64e7
allanizzi <allan_izzi@hotmail.com>
2022-10-12 18:52:10 +0200

    Section 2 et navbar
    Ajouts des cards, gestion responsive, et ajustement navbar mobile

--- js/navbar.js
+++ js/navbar.js
@@ -1,22 +1,27 @@
 (function($) { 
 
-  "use strict"; // Start of use strict
   
     $(function() { 
       $('nav ul li a:not(:only-child)').click(function(e) {
         $(this).siblings('.nav-dropdown').toggle();
         $('.dropdown').not($(this).siblings()).hide();
         e.stopPropagation();
+        
       });
       $('html').click(function() {
         $('.nav-dropdown').hide();
+        
       });
       $('#nav-toggle').click(function() {
         $('nav ul').slideToggle();
+        
       });
       $('#nav-toggle').on('click', function() {
         this.classList.toggle('active');
         document.getElementById("nav-bar").classList.toggle('full-height-nav');
+        document.getElementById("home").classList.toggle('disable-home-scroll');
       });
+      
+   
     }); 
   })(jQuery);

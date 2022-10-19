ae374259dccffd6ab32340d1b4015ae3a86de6cd
allanizzi <allan_izzi@hotmail.com>
2022-10-19 19:05:09 +0200

    responsive cards

--- js/functions.js
+++ js/functions.js
@@ -1,25 +1,25 @@
 
-  function handle(evnt){
-    let cardsProjets = document.getElementsByClassName("card-projet");
+//   function handle(evnt){
+//     let cardsProjets = document.getElementsByClassName("card-projet");
 
-    if ( document.documentElement.clientWidth >= 780 && document.documentElement.clientWidth < 1700) {
-      for (let i = 0; i < cardsProjets.length; i++) {
-        document.getElementsByClassName("card-projet")[i].style.width = parseInt((((document.getElementById("cards").offsetWidth - 20) / 2) - 6)) + "px";
-      } 
-    }
-    else if ( document.documentElement.clientWidth >= 1700) {
-        for (let i = 0; i < cardsProjets.length; i++) {
-          document.getElementsByClassName("card-projet")[i].style.width = parseInt((((document.getElementById("cards").offsetWidth - 40) / 3) - 8)) + "px";
-        } 
-      }
-    else {
-        for (let i = 0; i < cardsProjets.length; i++) {
-            document.getElementsByClassName("card-projet")[i].style.width = "100%";
-        }
-    }
-  }
+//     if ( document.documentElement.clientWidth >= 780 && document.documentElement.clientWidth < 1700) {
+//       for (let i = 0; i < cardsProjets.length; i++) {
+//         document.getElementsByClassName("card-projet")[i].style.width = parseInt((((document.getElementById("cards").offsetWidth - 20) / 2) - 6)) + "px";
+//       } 
+//     }
+//     else if ( document.documentElement.clientWidth >= 1700) {
+//         for (let i = 0; i < cardsProjets.length; i++) {
+//           document.getElementsByClassName("card-projet")[i].style.width = parseInt((((document.getElementById("cards").offsetWidth - 40) / 3) - 8)) + "px";
+//         } 
+//       }
+//     else {
+//         for (let i = 0; i < cardsProjets.length; i++) {
+//             document.getElementsByClassName("card-projet")[i].style.width = "100%";
+//         }
+//     }
+//   }
 
-    window.onresize = handle;
-    window.onload = handle;
+//     window.onresize = handle;
+//     window.onload = handle;
 
 

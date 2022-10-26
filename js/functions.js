c3bd214e3f840b4c44e81a88bbf1183cbbc668be
allanizzi <allan_izzi@hotmail.com>
2022-10-26 18:13:36 +0200

    ajustements carousel

--- js/functions.js
+++ js/functions.js
@@ -1,45 +1,9 @@
+function precedent() {
 
-//   function handle(evnt){
-//     let cardsProjets = document.getElementsByClassName("card-projet");
+    document.getElementsByClassName("carousel-photos")[0].scrollLeft -= 300;
+}
 
-//     if ( document.documentElement.clientWidth >= 780 && document.documentElement.clientWidth < 1700) {
-//       for (let i = 0; i < cardsProjets.length; i++) {
-//         document.getElementsByClassName("card-projet")[i].style.width = parseInt((((document.getElementById("cards").offsetWidth - 20) / 2) - 6)) + "px";
-//       } 
-//     }
-//     else if ( document.documentElement.clientWidth >= 1700) {
-//         for (let i = 0; i < cardsProjets.length; i++) {
-//           document.getElementsByClassName("card-projet")[i].style.width = parseInt((((document.getElementById("cards").offsetWidth - 40) / 3) - 8)) + "px";
-//         } 
-//       }
-//     else {
-//         for (let i = 0; i < cardsProjets.length; i++) {
-//             document.getElementsByClassName("card-projet")[i].style.width = "100%";
-//         }
-//     }
-//   }
-
-//     window.onresize = handle;
-//     window.onload = handle;
-
-function myFunction(event) {
-
-    // let y = event.deltaY;
-
-    // scroll down
-    // if (y > 0) {
-    //     document.getElementsByClassName("carousel-photos")[0].scrollLeft += 50;
-    // }
-    // // scroll up
-    // else {
-    //     document.getElementsByClassName("carousel-photos")[0].scrollLeft -= 50;
-    // }
-
-    // if (document.getElementsByClassName("carousel-photos")[0].scrollLeft == 0 ) {
-    //     document.getElementsByClassName("carousel-photos")[0].style.overscrollBehavior = "auto";
-    // }
-    // else {
-    //     document.getElementsByClassName("carousel-photos")[0].style.overscrollBehavior = "contain";
-    // }
+function suivant() {
+    document.getElementsByClassName("carousel-photos")[0].scrollLeft += 300;
 }
 

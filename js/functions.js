2d11090fcfa7b7e0e2b485bd1ae6b7acfe80d932
allanizzi <allan_izzi@hotmail.com>
2022-10-28 14:49:18 +0200

    remise avant test

--- js/functions.js
+++ js/functions.js
@@ -1,18 +1,17 @@
-// function precedent() {
+function precedent() {
 
-//     document.getElementsByClassName("carousel-photos")[0].scrollLeft -= 300;
-// }
+    document.getElementsByClassName("carousel-photos")[0].scrollLeft -= 300;
+}
 
-// function suivant() {
-//     document.getElementsByClassName("carousel-photos")[0].scrollLeft += 300;
-//     alert("hello");
-// }
+function suivant() {
+    document.getElementsByClassName("carousel-photos")[0].scrollLeft += 300;
+}
 
-(function($) {
-    "use strict"; // Start of use strict
+// (function($) {
+//     "use strict"; // Start of use strict
 
-$('.suivant').on('click touchstart', function() {
-    alert("allan");
-});
+// $('.suivant').on('click touchstart', function() {
+//     alert("allan");
+// });
 
-})(jQuery); // End of use strict
+// })(jQuery); // End of use strict

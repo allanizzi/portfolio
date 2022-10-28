cca5671cb63ce88f27457f732043ded6522b124a
allanizzi <allan_izzi@hotmail.com>
2022-10-28 18:06:29 +0200

    Ajout nouvelle méthode pour que le click fonctionne sur mobile et tablette

--- js/functions.js
+++ js/functions.js
@@ -13,7 +13,12 @@
 
     $('.images').on('click','.suivant', function(e){
         e.preventDefault();
-        alert("hello");
+        document.getElementsByClassName("carousel-photos")[0].scrollLeft += 300;
+    });
+
+    $('.images').on('click','.precedent', function(e){
+        e.preventDefault();
+        document.getElementsByClassName("carousel-photos")[0].scrollLeft -= 300;
     });
 
 })(jQuery); // End of use strict
